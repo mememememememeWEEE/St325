f129ea3ea5b7fca3abc20422ce42ba7ca9433c9e
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-02-09 21:43:51 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,16 +11,30 @@ function processHeader(header) {
 };
 
 
-
-function FetchHtml(URL) {
-	return fetch(URL)
-		.then((response) => response.text())
-		.then((data) => {
-			document.body.innerHTML = data;
-		})
-	.catch((error) => {
-		console.error(error);
-	});
+async function FetchHtml(url) {
+	try {
+		const res = await fetch(url);
+		const html = await res.text();
+		return html;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 }
 
-FetchHtml('https://en.wikipedia.org/wiki/FIFA_World_Cup');
+const wikipediaPage = "https://en.wikipedia.org/wiki/Web_API";
+let data;
+
+//example
+FetchHtml(wikipediaPage)
+  .then(html => {
+    data = html;
+	console.log(data)
+  })
+  .catch(error => {
+    console.error("An error occurred:", error);
+});
+
+//apparently this is undefined but the console.log above is valid
+console.log(data)
+//nvm the one above is an ASYNC function (ie it technically runs after this console.log)
